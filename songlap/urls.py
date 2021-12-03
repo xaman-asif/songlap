2bd9241b04168fd6b8a64b5947771623de70d14a
MRezan <shahriarasif753@gmail.com>
2021-12-03 12:24:52 +0600

    reply app connected

--- songlap/urls.py
+++ songlap/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('post.urls'))
+    path('',include('post.urls')),
+    path('reply/',include('reply.urls'))
 ]

43d1031040b6361d40da6be93317ff23e8b65765
Asif Jaman <aopi181295@bscse.uiu.ac.bd>
2021-12-03 16:09:41 +0600

    Created Post in Models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import activate
+from home.models import Categories
+
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    publish = models.DateTimeField()
+    
+    viewed = models.IntegerField()
+    replies = models.IntegerField()
+    latest = models.DateTimeField()
+
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)

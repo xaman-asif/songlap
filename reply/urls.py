4b93252ec65158c40376181d93d380ad2ec197a3
sabbir270 <sabbirsarkar27@gmail.com>
2022-01-11 00:25:34 +0600

    url path added for 'editreply'

--- reply/urls.py
+++ reply/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 
   path('/<int:id>',views.Reply,name='reply'),
-  path('delete_reply/',views.DeleteReply,name='delete_reply')
+  path('delete_reply/',views.DeleteReply,name='delete_reply'),
+  path('/editreply/<int:id>/<int:post_id>',views.editreply,name='editreply')
     
 ]
